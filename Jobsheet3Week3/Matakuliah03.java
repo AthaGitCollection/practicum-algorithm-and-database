5f89635b5f2bbbe6a494eb35aa5b78d5a80b466e
AthaHalilintar <Atharasya088@gmail.com>
2026-03-06 05:09:50 +0700

    3.4.1

---
+++ Jobsheet3Week3/Matakuliah03.java
@@ -0,0 +1,17 @@
+package Jobsheet3Week3;
+
+public class Matakuliah03 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah03 (String kode, String nama, int sks, int jumlaJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+
+    }
+    
+}
