094dc46cb6b9d8364f3bbe62ac89a494ff9a32d2
AthaHalilintar <Atharasya088@gmail.com>
2026-03-18 14:52:31 +0700

    Assignment

---
+++ Jobsheet5Week5/StudentNilai.java
@@ -0,0 +1,43 @@
+package Jobsheet5Week5;
+
+public class StudentNilai {
+    String name;
+    int uts, uas;
+
+    StudentNilai(String name, int uts, int uas){
+        this.name = name;
+        this.uts = uts;
+        this.uas = uas;
+    }
+
+    // 🔥 Divide & Conquer - MAX UTS
+    static int maxUTS(StudentNilai arr[], int l, int r){
+        if(l == r){
+            return arr[l].uts;
+        }
+        int mid = (l + r) / 2;
+        int left = maxUTS(arr, l, mid);
+        int right = maxUTS(arr, mid+1, r);
+        return Math.max(left, right);
+    }
+
+    // 🔥 Divide & Conquer - MIN UTS
+    static int minUTS(StudentNilai arr[], int l, int r){
+        if(l == r){
+            return arr[l].uts;
+        }
+        int mid = (l + r) / 2;
+        int left = minUTS(arr, l, mid);
+        int right = minUTS(arr, mid+1, r);
+        return Math.min(left, right);
+    }
+
+    // 🔥 Brute Force - AVG UAS
+    static double avgUAS(StudentNilai arr[]){
+        double total = 0;
+        for(int i=0; i<arr.length; i++){
+            total += arr[i].uas;
+        }
+        return total / arr.length;
+    }
+}
